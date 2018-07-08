3646699ead226f7b5d512744799ca862af8b1836
__zalgo__ <jromano416@gmail.com>
2018-07-07 18:30:04 -0700

    almost there, formatting is better but need to fix 1 more bug

--- main/roller/roller.py
+++ main/roller/roller.py
@@ -12,8 +12,9 @@ Can also do negative "penalties" the same way.
 
 import random
 import re
+import discord
 from discord.ext import commands
-from helpers import ROLLER_MESSAGES
+from helpers import ICON_URL, ROLLER_MESSAGES, class_colors
 
 
 class DiceRoller(object):
@@ -27,25 +28,35 @@ class DiceRoller(object):
             roll_string = re.search(r'^[(\d*)]{1,3}d[\d*]{1,3}$', roll_arg)
         return roll_string
 
-    def roll_dice(dice_count, dice_sides, bonus):
+    def roll_dice(dice_count, dice_sides, bonus, splitter):
         """Roll a dice for a random outcome.
 
-        number_of_dice is an int representing how many dice to roll.
-        dice_sides is an int representing how many sides on each die rolled.
-        bonus is an int representing how much to add/subtract from the roll.
+        rolled_dice is the returned dict.
+        number_of_dice int: represents how many dice to roll.
+        dice_sides int: represents how many sides on each die rolled.
+        bonus int: represents how much to add/subtract from the roll.
+        splitter string: denotes whether the bonus will be added or subtracted.
         """
-        roll_stats = {
+        rolled_dice = {
             'roll_results': [],
+            'individual_rolls': [],
             'total': 0,
             'bonus': 0,
         }
         for dice in range(0, dice_count):
             dice = random.randint(1, dice_sides)
-            roll_stats['roll_results'].append(dice)
-        roll_stats['total'] = int(sum(
-            roll_stats['roll_results']) + bonus)
-        roll_stats['bonus'] = bonus
-        return roll_stats
+            rolled_dice['roll_results'].append(dice)
+        for dice in rolled_dice['roll_results']:
+            rolled_dice['individual_rolls'].append((
+                str(dice), f"({rolled_dice['roll_results'].count(dice)})"))
+        unmodified_total = rolled_dice['total'] = int(sum(
+            rolled_dice['roll_results']))
+        if splitter == "+":
+            rolled_dice['total'] = int(unmodified_total + bonus)
+        elif splitter == "-":
+            rolled_dice['total'] = int(unmodified_total - bonus)
+        rolled_dice['bonus'] = bonus
+        return rolled_dice
 
 
 @commands.command(case_insensitive=True, pass_context=True)
@@ -57,6 +68,8 @@ async def roll(ctx, arg):
     roll_string = DiceRoller.parse_roll(updated_arg, roll_bonus)
     try:
         valid_roll = roll_string.string
+        roll_message = await ctx.send("```diff\n+ ROLLING...\n```")
+        splitter = ''
         if '-' in valid_roll:
             splitter = '-'
         elif '+' in valid_roll:
@@ -68,10 +81,24 @@ async def roll(ctx, arg):
         else:
             dice_sides = int(valid_roll.split('d')[1])
             dice_bonus = 0
-        rolled_dice = DiceRoller.roll_dice(dice_count, dice_sides, dice_bonus)
-        await ctx.send(
-            ROLLER_MESSAGES['success_result'].format(
-                rolled_dice['roll_results'], rolled_dice['bonus'],
-                rolled_dice['total']))
+        rolled_dice = DiceRoller.roll_dice(
+            dice_count, dice_sides, dice_bonus, splitter)
+        individual_dice_rolls = rolled_dice['individual_rolls']
+        embed = discord.Embed(
+            title="Results", description="----------------",
+            colour=class_colors['dice_roller'])
+        embed.set_author(
+            name=(f"{ctx.message.author} rolled "
+                  f"{dice_count}d{dice_sides}{splitter}{dice_bonus}"),
+            icon_url=f"{ICON_URL['main_logo']}")
+        embed.set_thumbnail(url=f"{ICON_URL['dice_roller']}")
+        embed.add_field(
+            name=f"Total (including bonus):", value=f"{rolled_dice['total']}",
+            inline=True)
+        embed.add_field(
+            name="Individual Dice Rolls:",
+            value=f"{individual_dice_rolls}", inline=True)
+        await roll_message.edit(content="", embed=embed)
     except AttributeError:  # Fail if valid_roll isn't created
-        await ctx.send(ROLLER_MESSAGES['invalid_roll'].format(arg))
+        await roll_message.edit(
+            content=ROLLER_MESSAGES['invalid_roll'].format(arg))
